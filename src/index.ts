5724c74d560bbc0ff1d6bb942df7bd0e565cfa65
andreiusatii06 <andrei.usatii@legitscript.com>
2024-11-08 15:42:28 +0200

    first commit

---
+++ src/index.ts
@@ -0,0 +1,2 @@
+// src/index.ts
+export const sum = (a: number, b: number) => a + b;
---
+++ tsup.config.ts
@@ -0,0 +1,13 @@
+// tsup.config.ts
+import "dotenv/config";
+import { defineConfig } from "tsup";
+
+export default defineConfig({
+  entry: {
+    index: "src/index.ts",
+  },
+  external: [],
+  format: ["esm", "cjs"],
+  esbuildPlugins: [],
+  dts: "src/index.ts",
+});
